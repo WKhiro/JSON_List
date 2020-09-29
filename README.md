743fd7db549864a49e39980365102dc3d6392399
Wesley Kok <ryutarro123@gmail.com>
2020-09-28 23:17:01 -0700

    Finished

--- README.md
+++ README.md
@@ -1,8 +1,5 @@
 # JSON List
 
-This was a short project made in about ? hours. This simple web application connects to this [JSON data](https://fetch-hiring.s3.amazonaws.com/hiring.json) and then fetches and sorts the data according to listId, and then name. Names that are blank or null are omitted.
+This was a short project made in about 2 hours. This simple web application connects to this [JSON data](https://fetch-hiring.s3.amazonaws.com/hiring.json) and then fetches and sorts the data according to listId, and then name. Names that are blank or null are omitted. Since the data is sorted by "name" and not "id", this would be an example of correct ordering (ascending): "101", "2", "500", "67".
+
 [Link to web application]()
-
-## Features
-
-## Potential Improvements
--- scripts.js
+++ scripts.js
@@ -1,26 +1,53 @@
-const proxyurl = "https://cors-anywhere.herokuapp.com/";
+// Wesley Kok
 
-fetch(proxyurl + "https://fetch-hiring.s3.amazonaws.com/hiring.json")
+const proxyurl = "https://cors-anywhere.herokuapp.com/";
+const jsonurl = "https://fetch-hiring.s3.amazonaws.com/hiring.json";
+
+fetch(proxyurl + jsonurl)
   .then((response) => {
     return response.json();
   })
   .then((data) => {
-    console.log(data);
+    var listId = 0;
+    var childContainer = document.getElementById("root");
+
+    // Sort by listId first, and then by "name", not "id"
+    data.sort(function (a, b) {
+      return a.listId - b.listId || ("" + a.name).localeCompare(b.name);
+    });
+
     for (const [key, value] of Object.entries(data)) {
-      console.log(key);
-      console.log(value);
-      console.log(value.name);
+      // Omit empty and null name values
       if (value.name) {
-        const h1 = document.createElement("h1");
-        const titleText = value.listId;
+        // Detect change in listId and make a new container
+        if (!listId || value.listId !== listId) {
+          const card = document.createElement("div");
+          card.setAttribute("class", "card");
 
-        h1.textContent = titleText;
-        const h5 = document.createElement("h5");
-        h5.textContent = value.name;
+          const listIdTitle = document.createElement("h1");
+          listIdTitle.setAttribute("class", "h1");
+          listIdTitle.textContent = "ListId " + value.listId;
 
-        h1.appendChild(h5);
-        var container = document.getElementById("parent");
-        container.appendChild(h1);
+          const newContainer = document.createElement("div");
+          newContainer.setAttribute("class", "container");
+          newContainer.setAttribute("id", value.listId);
+
+          card.appendChild(listIdTitle);
+          card.appendChild(newContainer);
+
+          const mainContainer = document.getElementById("root");
+          mainContainer.appendChild(card);
+
+          // Assign current listId to new listId
+          listId = value.listId;
+
+          childContainer = document.getElementById(value.listId);
+        }
+
+        // Add items to corresponding container
+        const itemName = document.createElement("h3");
+        itemName.textContent = value.name;
+        childContainer.appendChild(itemName);
       }
     }
   })
